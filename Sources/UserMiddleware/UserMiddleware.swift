2accf7c01444cb7455dae3d676dd0b66c901c4ac
Nick <npvisual@users.noreply.github.com>
2020-11-12 14:16:38 -0600

    First draft including protocol for repo to be able to do CRUD ops on user data.

--- Sources/UserMiddleware/UserMiddleware.swift
+++ Sources/UserMiddleware/UserMiddleware.swift
@@ -1,3 +1,122 @@
-struct UserMiddleware {
-    var text = "Hello, World!"
+import os.log
+import Foundation
+import Combine
+
+import SwiftRex
+
+// MARK: - ACTIONS
+public enum UserAction {
+    case create
+    case delete
+    case update
+    case read
+}
+
+public struct UserState: Codable, Equatable, Hashable {
+    public let beaconid: UInt16?
+    public let email: String?
+    public let givenName: String?
+    public let familyName: String?
+    public let displayName: String?
+    public let families: [String: Bool]?
+    public let tracking: Bool?
+    
+    public static let empty: UserState = .init()
+    
+    public init(
+        beaconid: UInt16? = nil,
+        email: String? = nil,
+        givenName: String? = nil,
+        familyName: String? = nil,
+        displayName: String? = nil,
+        families: [String: Bool]? = nil,
+        tracking: Bool? = true
+    )
+    {
+        self.beaconid = beaconid
+        self.email = email
+        self.givenName = givenName
+        self.familyName = familyName
+        self.displayName = displayName
+        self.families = families
+        self.tracking = tracking
+    }
+}
+
+// MARK: - ERRORS
+public enum UserError: Error {
+    case somethingSomething
+}
+
+// MARK: - PROTOCOL
+public protocol UserStorage {
+    func create() -> AnyPublisher<Void, UserError>
+    func read() -> AnyPublisher<Void, UserError>
+    func update() -> AnyPublisher<Void, UserError>
+    func delete() -> AnyPublisher<Void, UserError>
+}
+
+// MARK: - MIDDLEWARE
+public class UserMiddleware: Middleware {
+    public typealias InputActionType = UserAction
+    public typealias OutputActionType = UserAction
+    public typealias StateType = UserState
+    
+    private static let logger = OSLog(subsystem: Bundle.main.bundleIdentifier!, category: "UserMiddleware")
+
+    private var output: AnyActionHandler<OutputActionType>? = nil
+    private var getState: () -> StateType = {  StateType.empty }
+
+    private var provider: UserStorage
+
+    public init(provider: UserStorage) {
+        self.provider = provider
+    }
+    
+    public func receiveContext(getState: @escaping GetState<StateType>, output: AnyActionHandler<OutputActionType>) {
+        os_log(
+            "Receiving context...",
+            log: UserMiddleware.logger,
+            type: .debug
+        )
+        self.getState = getState
+        self.output = output
+
+    }
+    
+    public func handle(
+        action: InputActionType,
+        from dispatcher: ActionSource,
+        afterReducer : inout AfterReducer
+    ) {
+        switch action {
+        default:
+            os_log(
+                "Not handling this case : %s ...",
+                log: UserMiddleware.logger,
+                type: .debug,
+                String(describing: action)
+            )
+            break
+        }
+
+        afterReducer = .do { [self] in
+            let newState = getState()
+            os_log(
+                "Calling afterReducer closure...",
+                log: UserMiddleware.logger,
+                type: .debug
+            )
+            switch action {
+            default:
+                os_log(
+                    "Apparently not handling this case either : %s...",
+                    log: UserMiddleware.logger,
+                    type: .debug,
+                    String(describing: action)
+                )
+                break
+            }
+        }
+    }
 }
--- Tests/UserMiddlewareTests/UserMiddlewareTests.swift
+++ Tests/UserMiddlewareTests/UserMiddlewareTests.swift
@@ -6,7 +6,7 @@ final class UserMiddlewareTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(UserMiddleware().text, "Hello, World!")
+        // XCTAssertEqual(UserMiddleware().text, "Hello, World!")
     }
 
     static var allTests = [
